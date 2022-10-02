cec7f429b3f6c845460251800a28b2ebd718e4f9
AungAyeThan <aungayethan2015@gmail.com>
2022-10-02 13:04:59 +0700

    WIP: project directory setup

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"sheetpilot/sheet-pilot-api/internal"
+)
+
+var log *logrus.Entry
+
+func init() {
+	l := logrus.New()
+
+	log = l.WithFields(logrus.Fields{
+		"app": map[string]string{
+			"host": os.Getenv("HOST"),
+		},
+	})
+}
+
+func main() {
+
+	options := internal.Config{
+		ListenAddress:            os.Getenv("LISTEN_ADDRESS"),
+		SheetPilotServiceAddress: os.Getenv("SHEETPILOT_SERVICE_ADDRESS"),
+	}
+
+	svc, err := internal.New(context.Background(), log, options)
+	if err != nil {
+		log.WithError(err).Fatal("internal.New()")
+	}
+
+	svc.Start() // start app http.ListenAndServe
+
+}
